8f430644a5d726d8ee465abf0c9299efd4dbaaa5
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2018-07-19 14:59:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bnk-api-nodejs-express-be
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = `mongodb://${process.env.BNK_MONGO_USERNAME}:${process.env.BNK_MONGO_PASSWORD}@${process.env.BNK_MONGO_HOST}:${process.env.BNK_MONGO_PORT}/${process.env.BNK_MONGO_DB_NAME}`;
+var dbConnect = null;
+
+console.log(url);
+
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  if (err) throw err;
+  dbConnect = db.db("bnk");
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbConnect.collection("member").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+  });
+})
+
+app.listen(3000, () => console.log('Example app listening on PORT 3000!'))
