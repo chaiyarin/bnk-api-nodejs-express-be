2bc7f20750d580f365a4e048242ef02e543cba26
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2018-07-20 10:59:58 +0700

    refector code route to routing file

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const memberService = require('./member.service');
 const app = express();
+require('./routing')(app);
 app.use(cors());
 var port = process.env.PORT || 3000;
-
-app.get('/api/save', (req, res) => {
-    memberService.saveMember('ชัยรินทร์ เนียมสุวรรณ', 'คอนโดลุมพินี', res);
-})
-
-app.get('/api/get-members', (req, res) => {
-    memberService.getMembers(res);
-})
-
 app.listen(port, () => console.log('Example app listening on PORT 3000!'))
