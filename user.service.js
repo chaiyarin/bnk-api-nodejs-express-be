75083255814686cd218d7838b0c8a02e952831ad
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2018-07-21 19:56:16 +0700

    fix response message when user login and not found user and fix text login to lower case

--- user.service.js
+++ user.service.js
@@ -3,13 +3,13 @@ var jsonWebToken = require('jsonwebtoken');
 
 module.exports.login = function (req, res) {
     User.find({
-        login: req.body.login
+        login: req.body.login.toLowerCase()
     }, (err, result) => {
-        const userData = result[0];
-        if(err){
-            res.status(500);
-            res.json(err);
+        if(err || !result.length){
+            res.status(404);
+            res.send('Username และ Password ของท่านไม่ถูกต้อง กรุณาตรวจสอบข้อมูลอีกครั้ง');
         }else{
+            const userData = result[0];
             if(userData.password == req.body.password){
                 const token = jsonWebToken.sign({ name: userData.login, id: userData._id }, 'beer', { expiresIn: 3600 })
                 res.json({token: token});
@@ -20,14 +20,14 @@ module.exports.login = function (req, res) {
 
 module.exports.createUser = function (req, res) {
     User.create({
-        login: req.body.login,
+        login: req.body.login.toLowerCase(),
         password: req.body.password,
         display: req.body.display,
-        email: req.body.email
+        email: req.body.email.toLowerCase()
     }, (err, result) => {
         if(err) {
             res.status(500);
-            res.json({error_message: 'Cannot Create New User'});
+            res.send('ไม่สามารถสร้าง User ใหม่ได้กรุณาติดต่อผู้ดูแลระบบ');
         }else{
             res.json({message: 'Create User Successfully'})
         }
